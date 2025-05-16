dde98d24fd25a4bf69b932099771401afc8310bd
Amir <sagan.5050@gmail.com>
2025-05-16 05:45:35 +0300

    update swagger

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -813,7 +813,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "areeb.ddns.net:2025",
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "Event Booking API",
